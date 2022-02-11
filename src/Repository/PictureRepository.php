aae8dd7ebc7daed3f17469c50a44e8b442693c42
Jérémie Broutier <jeremie.broutier@posteo.net>
2022-02-11 09:24:14 +0100

    refactor: Remove unneeded repository methods.

--- src/Repository/PictureRepository.php
+++ src/Repository/PictureRepository.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Picture;
-use App\Repository\Traits\FilterableTrait;
-use App\Repository\Traits\SortableTrait;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Pagerfanta\Doctrine\ORM\QueryAdapter;
-use Pagerfanta\Pagerfanta;
 
 /**
  * @method Picture|null find($id, $lockMode = null, $lockVersion = null)
@@ -22,25 +18,8 @@ use Pagerfanta\Pagerfanta;
  */
 class PictureRepository extends ServiceEntityRepository
 {
-    use FilterableTrait;
-    use SortableTrait;
-
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Picture::class);
-    }
-
-    /**
-     * @param array<string, mixed> $filters
-     * @param array<string, string|null> $orderBy
-     * @return Pagerfanta<Picture>
-     */
-    public function findPaginated(array $filters = [], array $orderBy = []): Pagerfanta
-    {
-        $builder = $this->createQueryBuilder('p');
-        $this->applyFilters($builder, $filters);
-        $this->applyOrder($builder, $orderBy);
-
-        return new Pagerfanta(new QueryAdapter($builder));
     }
 }
