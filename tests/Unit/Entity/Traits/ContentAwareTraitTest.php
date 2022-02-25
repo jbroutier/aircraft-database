6fa424fbd923397b2164a4aa0b26af181a721a46
Jérémie Broutier <jeremie.broutier@posteo.net>
2022-02-25 13:16:41 +0100

    test: Add unit tests.

---
+++ tests/Unit/Entity/Traits/ContentAwareTraitTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Entity\Traits;
+
+use App\Entity\Interface\ContentAwareInterface;
+use App\Entity\Traits\ContentAwareTrait;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+final class ContentAwareEntity implements ContentAwareInterface
+{
+    use ContentAwareTrait;
+}
+
+final class ContentAwareTraitTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @testdox Method getContent() returns null by default.
+     */
+    public function testGetContent(): void
+    {
+        $entity = new ContentAwareEntity();
+
+        self::assertNull($entity->getContent());
+    }
+
+    /**
+     * @testdox Method setContent() sets the content.
+     * @noinspection SpellCheckingInspection
+     */
+    public function testSetContent(): void
+    {
+        $entity = new ContentAwareEntity();
+        $entity->setContent('Sint ea dolorem quia consequuntur accusamus.');
+
+        self::assertEquals('Sint ea dolorem quia consequuntur accusamus.', $entity->getContent());
+    }
+}
