d5c8d96c87eca093a71ab61c5187f1d2c385132c
Jérémie Broutier <jeremie.broutier@posteo.net>
2022-03-06 16:53:28 +0100

    chore: Format code.

--- migrations/Version20220217084458.php
+++ migrations/Version20220217084458.php
@@ -16,11 +16,7 @@ final class Version20220217084458 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql(
-            'ALTER TABLE picture ADD (
-                description VARCHAR(255) DEFAULT NULL
-            )'
-        );
+        $this->addSql('ALTER TABLE picture ADD description VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
