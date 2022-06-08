0fbae49f84aedc2603b180020f948745b81246f2
Jérémie Broutier <jeremie.broutier@posteo.net>
2022-06-08 15:56:05 +0200

    chore: Add CSS properties order check.

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,13 +3,15 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   plugins: [
     'stylelint-scss',
-    'stylelint-no-unsupported-browser-features'
+    'stylelint-no-unsupported-browser-features',
+    'stylelint-order'
   ],
   rules: {
     'at-rule-no-unknown': null,
     'function-no-unknown': null,
     'plugin/no-unsupported-browser-features': [true, {
       severity: 'warning'
-    }]
+    }],
+    'order/properties-alphabetical-order': true
   }
 }
